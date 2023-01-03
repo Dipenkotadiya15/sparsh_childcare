0497a701bb60d08a14b9affa1d0132994c82851d
Dipenkotadiya15 <dipen.adsum@gmail.com>
2023-01-03 18:12:56 +0530

    Day_2

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,4 +19,40 @@ $(document).ready(function() {
     //     }
     // })
 
+
+    
+    $("#owl-demo").owlCarousel({
+        nav: true,
+        dots: false,
+        animateOut: 'fadeOut',
+        items : 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        loop: true,
+        navText:[
+            '<i class="fa-solid fa-chevron-left"></i>',
+            '<i class="fa-solid fa-chevron-right"></i>'
+        ]
+    });
+
 })
+
+
+let scrollpos = window.scrollY;
+	const header = document.querySelector(".navbar");
+	const header_height = header.offsetHeight;
+
+	window.addEventListener('scroll', function () {
+		scrollpos = window.scrollY;
+		if (scrollpos >= header_height + 100) {
+			console.log(header_height);
+			header.classList.add('sticky');
+		} else {
+			header.classList.remove('sticky');
+		}
+	})
+
+
+
+    
