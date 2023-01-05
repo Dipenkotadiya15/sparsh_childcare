ff6dba6851d42877e6eed254af51681d269fef48
Dipenkotadiya15 <dipen.adsum@gmail.com>
2023-01-05 18:19:23 +0530

    Day_4

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,6 +36,59 @@ $(document).ready(function() {
         ]
     });
 
+    $("#owl-demo2").owlCarousel({
+        nav: false,
+        dots: false,
+        margin: 50,
+        items : 1,
+        autoplay:true,
+        autoplayTimeout:7000,
+        autoplayHoverPause:false,
+        loop: true,
+       
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            576:{
+                items:2,
+            },
+            768:{
+                items:3,
+            },
+            992:{
+                items:4,
+            },
+            1200:{
+                items:5,
+            }
+        }
+    });
+
+    $("#owl-demo3").owlCarousel({
+        
+        dots: false,
+        items : 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        loop: true,
+        navText:[
+            '<i class="fa-solid fa-chevron-left"></i>',
+            '<i class="fa-solid fa-chevron-right"></i>'
+        ],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                nav:false
+            },
+            992:{     
+                nav:true
+            }
+        }
+    });
+
 })
 
 
@@ -45,7 +98,7 @@ let scrollpos = window.scrollY;
 
 	window.addEventListener('scroll', function () {
 		scrollpos = window.scrollY;
-		if (scrollpos >= header_height + 100) {
+		if (scrollpos >= header_height + 800) {
 			console.log(header_height);
 			header.classList.add('sticky');
 		} else {
