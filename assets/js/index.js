c388425abe25ad6a39540613a4665103674c33a0
Dipenkotadiya15 <dipen.adsum@gmail.com>
2023-01-06 18:24:42 +0530

    Day_5

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
         nav: true,
         dots: false,
         animateOut: 'fadeOut',
+        autoHeight: true,
         items : 1,
         autoplay:true,
         autoplayTimeout:5000,
@@ -89,7 +90,42 @@ $(document).ready(function() {
         }
     });
 
+    $("#owl-demo4").owlCarousel({
+        dots: false,
+        items : 3,
+        margin:20,
+        center: true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        loop: true,
+        navText:[
+            '<i class="fa-solid fa-chevron-left"></i>',
+            '<i class="fa-solid fa-chevron-right"></i>'
+        ],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                nav:false
+            },
+            992:{     
+                nav:false
+            }
+        }
+    })
+
+    // .on('click', '.owl-item', function () {
+    //     $('#owl-demo3').trigger('to.owl.carousel', [$(this).index(), duration, true]);
+    // })
+
 })
+
+// $(document).on('click', 'second-slider.owl-item', function() {
+//     owlIndex = $(this).index();
+//     count = document.querySelectorAll(".owl-item.active").length;
+//     $('.first-slider').trigger('to.owl.carousel', owlIndex - count + 1);
+//   });
+
 
 
 let scrollpos = window.scrollY;
