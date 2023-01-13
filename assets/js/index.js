0bfd182ada85babfbe0729ccdf41da8e0e5e50c9
Dipenkotadiya15 <dipen.adsum@gmail.com>
2023-01-13 12:29:56 +0530

    - Happy Patient Section is Complete - Sticky Header is Complete - Sparsh Childcare weebsite is Done

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
         margin: 50,
         items: 1,
         autoplay: true,
-        autoplayTimeout: 7000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: false,
         loop: true,
 
@@ -45,66 +45,7 @@ $(document).ready(function () {
             }
         }
     });
-
-    $("#owl-demo3").owlCarousel({
-
-        dots: false,
-        nav: true,
-        items: 1,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: false,
-        loop: true,
-        navText: [
-            '<i class="fa-solid fa-chevron-left"></i>',
-            '<i class="fa-solid fa-chevron-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0: {
-                nav: false
-            },
-            992: {
-                nav: true
-            }
-        }
-    });
-
-    $("#owl-demo4").owlCarousel({
-        dots: false,
-        items: 3,
-        margin: 20,
-        center: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: false,
-        loop: true,
-        navText: [
-            '<i class="fa-solid fa-chevron-left"></i>',
-            '<i class="fa-solid fa-chevron-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0: {
-                nav: false
-            },
-            992: {
-                nav: false
-            }
-        }
-    })
-
-    // .on('click', '.owl-item', function () {
-    //     $('#owl-demo3').trigger('to.owl.carousel', [$(this).index(), duration, true]);
-    // })
-
 })
-
-// $(document).on('click', 'second-slider.owl-item', function() {
-//     owlIndex = $(this).index();
-//     count = document.querySelectorAll(".owl-item.active").length;
-//     $('.first-slider').trigger('to.owl.carousel', owlIndex - count + 1);
-//   });
 
 
 // STICKY HEADER :-
@@ -122,19 +63,7 @@ window.addEventListener('scroll', function () {
     }
 })
 
-
-
-// SCROLL TO TOP :-
-// const toTop = document.querySelector('.to-top');
-
-// window.addEventListener('sccroll', () => {
-//     if (window.pageYOffset > 100) {
-//         toTop.classList.add('active');
-//     } else {
-//         toTop.classList.remove('active');
-//     }
-// })    
-
+// SCROLL TO TOP 
 $(window).scroll(function (e) {
     if ($(this).scrollTop() > 0) {
         $(".to-top").css("display", "block");
@@ -143,3 +72,77 @@ $(window).scroll(function (e) {
     }
 });
 
+// Patients owl-Carousel :-
+$(document).ready(function () {
+
+    if ($('.client-owl-carousel').length && $('.thumb-owl-carousel').length) {
+
+        var $sync3 = $(".client-owl-carousel"),
+            $sync4 = $(".thumb-owl-carousel"),
+            flag = false,
+            duration = 500;
+
+
+        $sync3.owlCarousel({
+                dots: false,
+                nav: true,
+                items: 1,
+                autoplay: true,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: false,
+                loop: true,
+                navText: [
+                    '<i class="fa-solid fa-chevron-left"></i>',
+                    '<i class="fa-solid fa-chevron-right"></i>'
+                ],
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        nav: false
+                    },
+                    992: {
+                        nav: true
+                    }
+                }
+            })
+
+            .on('changed.owl.carousel', function (e) {
+                if (!flag) {
+                    flag = false;
+                    $sync4.trigger('to.owl.carousel', [e.item.index, duration, true]);
+                    flag = false;
+                }
+            });
+
+
+        $sync4.owlCarousel({
+                dots: false,
+                nav: false,
+                items: 3,
+                margin: 20,
+                center: true,
+                autoWidth: false,
+                autoplay: true,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: false,
+                loop: true,
+                navText: [
+                    '<i class="fa-solid fa-chevron-left"></i>',
+                    '<i class="fa-solid fa-chevron-right"></i>'
+                ]
+            })
+
+            .on('click', '.owl-item', function () {
+                $sync3.trigger('to.owl.carousel', [$(this).index(), duration, true]);
+            })
+
+            .on('changed.owl.carousel', function (e) {
+                if (!flag) {
+                    flag = true;
+                    $sync3.trigger('to.owl.carousel', [e.item.index, duration, true]);
+                    flag = false;
+                }
+            });
+
+    }
+})
